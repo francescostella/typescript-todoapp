70b61ffbea7c165982b6221288342a6b39c6b393
Francesco Stella <francescostella@users.noreply.github.com>
2021-09-28 22:46:41 +0200

    feat: implement TodoListComponent logic

--- src/TodoListComponent.ts
+++ src/TodoListComponent.ts
@@ -7,4 +7,33 @@ export default class TodoListComponent {
   constructor(element: HTMLElement) {
     this.$element = $(element)
   }
+
+  render(todos) {
+    this.$element.html('')
+
+    if (!todos.length) {
+      this.$element.html(`<strong>You've completed everything you needed to do!</strong>`)
+      return
+    }
+
+    for (let index in todos) {
+      let todo = todos[index]
+      this.renderTodo(todo).appendTo(this.$element)
+    }
+  }
+
+  private renderTodo(todo) {
+    return $(`
+      <label class="list-group-item d-flex gap-3">
+        <input class="form-check-input flex-shrink-0" type="checkbox" value="" ${todo.state === 2 ? 'checked=""' : '' } style="font-size: 1.375em;">
+        <span class="pt-1 form-checked-content">
+          <strong>${todo.name}</strong>
+        </span>
+      </label>
+    `).on('click', function() {
+      var event = document.createEvent('CustomEvent');
+      event.initCustomEvent('todo-toggle', true, true, { todoId: todo.id });
+      this.dispatchEvent(event);
+    });
+  }
 }
