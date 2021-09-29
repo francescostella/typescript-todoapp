00b78aa321f8c69ba80de9ddf3a6f7dcfb346aa8
Francesco Stella <francescostella@users.noreply.github.com>
2021-09-29 16:44:07 +0200

    feat: implement TodoService interface

--- src/TodoService.ts
+++ src/TodoService.ts
@@ -1,4 +1,13 @@
-import { Todo } from './Model';
+import { Todo } from './Model'
+
+export interface ITodoService {
+  add(todo: Todo): Todo
+  add(todo: string): Todo
+  clearCompleted(): void
+  getAll(): Todo[]
+  getById(todoId: number): Todo
+  toggle(todoId: number): void
+}
 
 export default class TodoService {
   todos: Todo[]
